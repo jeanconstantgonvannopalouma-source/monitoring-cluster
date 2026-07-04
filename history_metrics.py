6d41defa838360b9b172ca645a5da6c9e7eebfec
jeanconstantgonvannopalouma-source <jeanconstantgonvannopalouma@gmail.com>
2026-07-04 20:50:54 +0200

    Add history_metrics.py with initial metric functions

---
+++ history_metrics.py
@@ -0,0 +1,16 @@
+# history_metrics.py
+
+def charger_metrics():
+    return []
+
+def extraire_latence_moyenne(metrics):
+    return None
+
+def extraire_disponibilite(metrics):
+    return None
+
+def extraire_uptime(metrics):
+    return None
+
+def extraire_anomalies(metrics):
+    return []
